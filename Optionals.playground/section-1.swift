896fc3dca5d304d293632eb0149c96e96e5e4902
Chris Eidhof <chris@eidhof.nl>
2015-01-07 11:06:31 +0100

    Optionals playground

---
+++ Optionals.playground/section-1.swift
@@ -0,0 +1,19 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+
+var optionalNumber: Int? = 3
+optionalNumber = nil
+
+let cities = ["Paris": 2243, "Madrid": 3216, "Amsterdam": 881, "Berlin": 3397]
+let capitals = ["France": "Paris", "Spain": "Madrid",
+               "The Netherlands": "Amsterdam", "Sweden": "Stockholm"]
+
+func populationOfCapital(countryName: String) -> Int? {
+    // TODO
+    return nil
+}
+
+
+populationOfCapital("France") // should return { Some 2243}
